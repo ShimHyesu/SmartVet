73e350abaacea1151ece89d954cd9a29463adf52
ShimHyesu <hesushim@gmail.com>
2021-12-03 01:01:16 +0900

    로딩화면 구현

--- screen/LoadingContainer.js
+++ screen/LoadingContainer.js
@@ -3,7 +3,7 @@ import { View, Text,Button,TouchableOpacity,Image} from 'react-native';
 
 export default function LoadingContainer({navigation}){
     return(
-        <TouchableOpacity onPress={() => navigation.navigate('logjoin_navi',{screen:'logjoin'})}>
+        <TouchableOpacity activeOpacity={1} onPress={() => navigation.navigate('logjoin_navi',{screen:'logjoin'})}>
             <Image 
             style={{ width: '100%', height: '100%',resizeMode:'cover' }}
             source={require('../asset/loading.png')}/>
